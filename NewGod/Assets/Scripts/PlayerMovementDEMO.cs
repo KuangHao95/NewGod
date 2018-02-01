1b1b6785934a1b6c83fd667a93d3f1b1fbe94f7e
Renguang Han <413526182@qq.com>
2018-02-01 14:29:13 +0800

    hrg2.1-2

--- NewGod/Assets/Scripts/PlayerMovementDEMO.cs
+++ NewGod/Assets/Scripts/PlayerMovementDEMO.cs
@@ -3,21 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementDEMO : MonoBehaviour {
-    private Rigidbody rig;
-    private float speed = 20f;
-    private float RotationSpeed = 100f;
-    // Use this for initialization
-    void Start () {
-        rig = GetComponent<Rigidbody>();
-    }
+    private float speed = 10f;
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float lr = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        Vector3 diffPos = new Vector3(lr, 0, translation);
+        float vertical = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        float horizontal = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
 
-        transform.Translate(diffPos);
-        //transform.Rotate(Vector3.up, lr);
+		transform.position += vertical * transform.forward + horizontal * transform.right;
     }
 }
